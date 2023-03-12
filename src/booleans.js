f859eb13424d6a98872aab79884f49e6d0cb56d8
Anthony_Penketh <a.p98@hotmail.com>
2023-03-12 17:02:20 +0000

    completed remaining 10 boolean tests

--- src/booleans.js
+++ src/booleans.js
@@ -25,42 +25,60 @@ function one(a, b) {
 
 function truthiness(a) {
   // your code here
+  return !!a;
 };
 
 function isEqual(a, b) {
   // your code here
+  return a === b;
 };
 
 function isGreaterThan(a, b) {
   // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
   // your code here
+  if (a % 2 == 0) {
+    return false
+  } else {
+    return true
+  }
 };
 
 function isEven(a) {
   // your code here
+  if (a % 2 == 0) {
+    return true
+  } else {
+    return false
+  }
 };
 
 function isSquare(a) {
   // your code here
+  return Math.sqrt(a) % 1 == 0
 };
 
 function startsWith(char, string) {
   // your code here
+  return char == string.charAt(0);
 };
 
 function containsVowels(string) {
   // your code here
+  return /[aeiou]/i.test(string);
 };
 
 function isLowerCase(string) {
   // your code here
+  return string == string.toLowerCase();
 };
 
 module.exports = {
