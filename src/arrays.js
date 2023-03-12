e664dfbf547d68eff4ef57c6527fb184c5cea521
Anthony_Penketh <a.p98@hotmail.com>
2023-03-12 18:15:41 +0000

    completed first 5 arrays tests

--- src/arrays.js
+++ src/arrays.js
@@ -1,21 +1,26 @@
 const getNthElement = (index, array) => {
   // your code here
+  return array[index % array.length];
 };
 
 const arrayToCSVString = array => {
   // your code here
+  return array.toString();
 };
 
 const csvStringToArray = string => {
   // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
   // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
   // your code here
+  return array.concat(element);
 };
 
 const removeNthElement = (index, array) => {
